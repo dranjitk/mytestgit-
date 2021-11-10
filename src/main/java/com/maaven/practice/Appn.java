eb80f631d4e28b71b5e2ea8fe72a59646b1502e6
ranjitdirisala <dranjit1913@gmail.com>
2021-11-10 11:10:37 +0530

    first commit

---
+++ src/main/java/com/maaven/practice/Appn.java
@@ -0,0 +1,13 @@
+package com.maaven.practice;
+
+/**
+ * Hello world!
+ *
+ */
+public class Appn 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
